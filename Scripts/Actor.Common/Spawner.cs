71767dfc1b3801e3ab050abd8f2f6cac31de1e0c
Warwick Molloy <wazzam@lokeldigital.visualstudio.com>
2017-08-04 08:02:05 +1000

    Initial commit

---
+++ Scripts/Actor.Common/Spawner.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Spawner Unity Component
+ * (c) Copyright 2017, Warwick Molloy
+ * GitHub account WazzaMo
+ * Provided under the terms of the MIT License.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Actor.Common {
+
+    public class Spawner : MonoBehaviour {
+        [SerializeField]
+        private GameObject CharacterPrefabToSpawn;
+
+        private Transform _SpawnTransform;
+
+        void Awake() {
+            SetupAttachedGameObjectAsSpawnPoint();
+        }
+
+        public GameObject SpawnCharacter() {
+            GameObject CharacterInWorld = MakeMainCharacter();
+            Transform CharacterTransform = CharacterInWorld.GetComponent<Transform>();
+            UpdateCharacterPosition(CharacterTransform);
+
+            return CharacterInWorld;
+        }
+
+        private void SetupAttachedGameObjectAsSpawnPoint() {
+            _SpawnTransform = GetComponent<Transform>();
+        }
+
+        private GameObject MakeMainCharacter() {
+            return GameObject.Instantiate(CharacterPrefabToSpawn, _SpawnTransform.position, _SpawnTransform.rotation);
+        }
+
+        private void UpdateCharacterPosition(Transform characterTransform) {
+            if (characterTransform != null) {
+                characterTransform.position = _SpawnTransform.position;
+                characterTransform.rotation = _SpawnTransform.rotation;
+            }
+        }
+    }
+
+}
