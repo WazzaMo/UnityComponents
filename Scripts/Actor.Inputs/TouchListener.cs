40e35a3c108a1b62e21111da1bd17ba91d6f9a50
Warwick Molloy <wazzam@lokeldigital.visualstudio.com>
2017-08-08 07:51:39 +1000

    First touch components

---
+++ Scripts/Actor.Inputs/TouchListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Actor.Inputs {
+
+    public class TouchListener : MonoBehaviour {
+        private Text _DebugTextBox;
+
+        void Start() {
+            _DebugTextBox = GetComponent<Text>();
+        }
+
+        public bool IsReady { get { return _DebugTextBox != null; } }
+
+        public void TouchEvent(float domainValue) {
+            var message = string.Format("TouchEvent [{0}]", domainValue);
+            ShowLine(message);
+        }
+
+        private void ShowLine(string message) {
+            if (IsReady) {
+                _DebugTextBox.text = message + "\n" + _DebugTextBox.text;
+            }
+        }
+    }
+
+}
---
+++ Scripts/Actor.Inputs/TouchRegion.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+using System.Linq;
+
+namespace Actor.Inputs {
+
+    [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(RectTransform))]
+    public class TouchRegion : MonoBehaviour {
+        public enum Direction {
+            Vertical,
+            Horizontal
+        }
+
+        [SerializeField] private Direction TouchDirection = Direction.Horizontal;
+        [SerializeField] private TouchListener[] TouchEventListeners = new TouchListener[0];
+        [SerializeField] private Camera TouchCamera;
+
+        private Image _TouchRegion;
+        private RectTransform _TouchRect;
+
+        void Start() {
+            SetupRegion();
+        }
+
+        void Update() {
+            if (IsReady) {
+                foreach(Touch aTouch in FindRelevantTouches()) {
+                    foreach(var listener in TouchEventListeners) {
+                        listener.TouchEvent(DomainValueFor(aTouch));
+                    }
+                }
+            }
+        }
+
+        public bool IsReady {
+            get {
+                return _TouchRegion != null && _TouchRect != null
+                && TouchEventListeners != null && TouchEventListeners.Length > 0
+                && TouchCamera != null
+                ;
+            }
+        }
+
+        private float DomainValueFor(Touch aTouch) {
+            Vector2 rectanglePos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_TouchRect, aTouch.position, TouchCamera, out rectanglePos);
+            float axisValue = TouchDirection == Direction.Horizontal ? rectanglePos.x : rectanglePos.y;
+            float axisRange = TouchDirection == Direction.Horizontal ? _TouchRect.rect.width : _TouchRect.rect.height;
+            return axisValue / axisRange;
+        }
+
+        private IEnumerable<Touch> FindRelevantTouches() {
+            return Input.touches.Where(tch =>
+               tch.phase != TouchPhase.Ended
+               && tch.phase != TouchPhase.Canceled
+               && IsInRegion(tch)
+            );
+        }
+
+        private bool IsInRegion(Touch touch) {
+            return RectTransformUtility.RectangleContainsScreenPoint(_TouchRect, touch.position, TouchCamera);
+        }
+
+        private void SetupRegion() {
+            _TouchRegion = GetComponent<Image>();
+            _TouchRect = GetComponent<RectTransform>();
+            if (! IsReady ) {
+                Debug.LogWarningFormat("GameObject {0} needs UnityEngine.UI Image, RectTransform and values for TouchListeners and the Camera.");
+            }
+        }
+    }
+
+}
