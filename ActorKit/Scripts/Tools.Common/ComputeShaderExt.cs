cf7054c13da95c2f74abf3b613c6c7ea30833263
Warwick Molloy <wazzam@lokeldigital.com>
2018-05-29 18:54:59 +1000

    Added compute shader extension and util classes

---
+++ ActorKit/Scripts/Tools.Common/ComputeShaderExt.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * ComputeShaderExt Unity Component
+ * (c) Copyright 2017, Warwick Molloy
+ * GitHub repo WazzaMo/UnityComponents
+ * Provided under the terms of the MIT License.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace Tools.Common {
+
+    public static class ComputeShaderExt {
+        const string EXTNAME = "ComputeShaderExt";
+
+        public static int FindKernelOrWarn(this ComputeShader compute, string kernalName, ref bool IsReady) {
+            if (IsReady) {
+                if (compute.HasKernel(kernalName)) {
+                    int kernel = compute.FindKernel(kernalName);
+                    IsReady = true;
+                    return kernel;
+                } else {
+                    IsReady = false;
+                    Logging.Warning("{0}: Can't find Compute Shader Kernel of name {1}", EXTNAME, kernalName);
+                    return -1;
+                }
+            } else {
+                Logging.Log("{0}: Not attempting to find Kernel - caller was not ready state!", EXTNAME);
+                return -1;
+            }
+        }
+    }
+
+}
---
+++ ActorKit/Scripts/Tools.Common/ComputeShaderUtil.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * ComputeShaderUtil Unity Component
+ * (c) Copyright 2017, Warwick Molloy
+ * GitHub repo WazzaMo/UnityComponents
+ * Provided under the terms of the MIT License.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+
+namespace Tools.Common {
+
+    public static class ComputeShaderUtil {
+        public static ComputeShader FindComputeOrWarn(string resourcePath, out bool IsReady) {
+            var shader = Resources.Load<ComputeShader>(resourcePath);
+            if (shader == null) {
+                Logging.Warning(typeof(ShaderExt), "Could not load shader: {0}", resourcePath);
+            } else {
+                Logging.Log(typeof(ShaderExt), "Found compute shader: {0}", resourcePath);
+            }
+            IsReady = shader != null;
+            return shader;
+        }
+    }
+
+}
